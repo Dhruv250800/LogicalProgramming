9c16d47e1bfcc93a41984ef5d27621ed3b4c0968
DhruvGour <dhruv250800@gmail.com>
2023-06-05 01:41:28 +0530

    [DhruvGour]-adding FibonaaciSeriesCode

---
+++ FibonacciGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+    using System;
+
+    public class FibonacciGenerator
+    {
+        private int previous;
+        private int current;
+
+        public FibonacciGenerator()
+        {
+            previous = 0;
+            current = 1;
+        }
+
+        public int Next()
+        {
+            int next = previous + current;
+            previous = current;
+            current = next;
+            return current;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter number");
+        int n = Convert.ToInt16(Console.ReadLine());
+
+        FibonacciGenerator fibonacci = new FibonacciGenerator();
+        for (int i = 0; i < n; i++)
+        {
+            int term = fibonacci.Next();
+            Console.Write(term + " ");
+
+
+        }
+
+        int number = 28;
+
+        bool isPerfect = PerfectNumberChecker.IsPerfectNumber(number);
+
+        if (isPerfect)
+            Console.WriteLine(number + " is a perfect number.");
+        else
+            Console.WriteLine(number + " is not a perfect number.");
+
+
+
+
+        int number1 = 17;
+
+        bool isPrime = PrimeNumberChecker.IsPrimeNumber(number1);
+
+        if (isPrime)
+            Console.WriteLine(number1 + " is a prime number.");
+        else
+            Console.WriteLine(number1 + " is not a prime number.");
+
+
+
+        int number2 = 12345;
+
+        int reversedNumber = NumberReverser.ReverseNumber(number2);
+
+        Console.WriteLine("Original Number: " + number2);
+        Console.WriteLine("Reversed Number: " + reversedNumber);
+
+
+
+        int[] couponNumbers = { 1, 2, 3, 4, 5 };
+
+        int totalRandomNumbersNeeded = CouponNumberGenerator.GenerateDistinctCouponNumbers(couponNumbers);
+
+        Console.WriteLine("Total random numbers needed: " + totalRandomNumbersNeeded);
+
+
+
+
+
+
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+
+        // Simulating some time-consuming operation
+        for (int i = 0; i < 1000000000; i++) { }
+
+        stopwatch.Stop();
+
+        stopwatch.DisplayElapsedTime();
+    }
+}
+
+
+
